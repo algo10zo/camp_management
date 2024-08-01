ea077b49ca28243abe31b28286c2eda658de2501
kikye04040 <kikye04040@gmail.com>
2024-08-01 16:49:59 +0900

    Student field create

--- src/camp/model/Student.java
+++ src/camp/model/Student.java
@@ -1,9 +1,16 @@
 package camp.model;
 
+import java.util.List;
 import java.util.Map;
 
 public class Student {
+    private String originalNumber;
+    private String studentName;
+    private List<String> subjectList;
 
-    public Student(String sequence, String studentName) {
+    public Student(String sequence, String studentName, List<String> subjectList) {
+        this.originalNumber = sequence;
+        this.studentName = studentName;
+        this.subjectList = subjectList;
     }
 }
---
+++ src/camp/model/StudentManagement.java
@@ -0,0 +1,8 @@
+package camp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManagement {
+    List<Student> studentList = new ArrayList<>();
+}

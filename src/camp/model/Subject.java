2cf62c4ac36f94c186a97d867e1f347e0ff1fd75
kikye04040 <kikye04040@gmail.com>
2024-08-01 16:22:53 +0900

    Subject field create

--- src/camp/model/Subject.java
+++ src/camp/model/Subject.java
@@ -1,6 +1,6 @@
 package camp.model;
 
-public class Subject {
+public class Subject implements ISubject {
     private String subjectId;
     private String subjectName;
     private String subjectType;
@@ -24,4 +24,28 @@ public class Subject {
         return subjectType;
     }
 
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public void addScore(int round, int score) {
+
+    }
+
+    @Override
+    public void updateScore(int round, int score) {
+
+    }
+
+    @Override
+    public String getGrade(int round) {
+        return "";
+    }
+
+    @Override
+    public double getAverageGrade() {
+        return 0;
+    }
 }

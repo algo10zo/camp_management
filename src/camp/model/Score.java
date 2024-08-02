bd06aff46e3b6ba2fd41ec0adbe3143b7034d3aa
Woojin1123 <boom175839@gmail.com>
2024-08-02 10:01:23 +0900

    Feat : Score class 점수 등록 등급 설정 구현

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -3,18 +3,65 @@ package camp.model;
 import java.util.Map;
 
 public class Score {
-    private int id;
+    private String id;
     private Map<Integer, String> scores; // <점수 , 등급> 으로 저장
 
-    public void setScores(Map<Integer, String> scores) {
-        this.scores = scores;
+    public Map<Integer, String> getScores() {
+        return scores;
     }
 
-    public int getId() {
+    public void scoreRegister(int score, String type) {
+        scores.put(score, setRank(score, type));
+    }
+
+    private String setRank(int score, String subjectType) {
+        if (subjectType.equals("필수")) {
+            if (score >= 95) {
+                return "A";
+            }
+            if (score < 95 && score >= 90) {
+                return "B";
+            }
+            if (score >= 80 && score < 90) {
+                return "C";
+            }
+            if (score >= 70 && score < 80) {
+                return "D";
+            }
+            if (score >= 60 && score < 70) {
+                return "F";
+            }
+            if (score < 60) {
+                return "N";
+            }
+        } else if (subjectType.equals("선택")) {
+            if (score >= 90) {
+                return "A";
+            }
+            if (score < 90 && score >= 80) {
+                return "B";
+            }
+            if (score >= 70 && score < 80) {
+                return "C";
+            }
+            if (score >= 60 && score < 70) {
+                return "D";
+            }
+            if (score >= 50 && score < 60) {
+                return "F";
+            }
+            if (score < 50) {
+                return "N";
+            }
+        }
+        throw new IllegalArgumentException("올바르지 않은 과목 타입입니다.")
+    }
+
+    public String getId() {
         return this.id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 }
---
+++ src/camp/model/ScoreManager.java
@@ -0,0 +1,8 @@
+package camp.model;
+
+public class ScoreManager {
+    public void init(){
+        System.out.println("");
+    }
+    public void
+}

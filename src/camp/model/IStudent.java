fec5b226d1dfb997c9f291917eddb61356872965
seungjin88 <powerjinny@hanmail.net>
2024-08-02 11:59:18 +0900

    Student

--- src/camp/model/IStudent.java
+++ src/camp/model/IStudent.java
@@ -3,10 +3,10 @@ package camp.model;
 import java.util.Map;
 
 public interface IStudent {
-    int getId();
+    String getId();
     String getName();
     void setName(String name);
-    String getStatus();
-    void setStatus(String status);
+//    String getStatus();
+//    void setStatus(String status);
     Map<String, ISubject> getSubjects();
 }

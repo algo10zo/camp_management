fae2a593c805f3ce1b93df213a1e8d4c760dbc2a
seungjin88 <powerjinny@hanmail.net>
2024-08-05 20:34:50 +0900

    ãsudent 구현

--- src/camp/model/Student.java
+++ src/camp/model/Student.java
@@ -5,49 +5,49 @@ import java.util.List;
 import java.util.Map;
 
 public class Student implements IStudent{
-    private String studentID; // 변경된 필드명
+    private final String studentID; // 변경된 필드명
     private String name;
     private String status;
     private Map<String, ISubject> subjects;
 
     public Student(String sequence, String name, List<String> subjects) {
-            this.studentID = sequence;
-            this.name = name;
-            this.status = String.valueOf(status);
-            this.subjects = new HashMap<String, ISubject>();
-        }
+        this.studentID = sequence;
+        this.name = name;
+        this.status = String.valueOf(status);
+        this.subjects = new HashMap<>();
+    }
 
     public Student(String sequence, String studentName) {
         this.studentID = sequence;
     }
 
     @Override
-        public String getStudentID () {
-            return studentID;
-        }
-
-        @Override
-        public String getName () {
-            return name;
-        }
-
-        @Override
-        public void setName (String name){
-            this.name = name;
-        }
-
-        @Override
-        public String getStatus () {
-            return status;
-        }
-
-        @Override
-        public void setStatus (String status){
-            this.status = status;
-        }
-
-        @Override
-        public Map<String, ISubject> getSubjects () {
-            return subjects;
-        }
+    public String getStudentID () {
+        return studentID;
     }
+
+    @Override
+    public String getName () {
+        return name;
+    }
+
+    @Override
+    public void setName (String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getStatus () {
+        return status;
+    }
+
+    @Override
+    public void setStatus (String status){
+        this.status = status;
+    }
+
+    @Override
+    public Map<String, ISubject> getSubjects () {
+        return subjects;
+    }
+}

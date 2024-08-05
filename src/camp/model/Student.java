316a25a8606c5d241e7a036aae42ca4044ec1ec3
seungjin88 <powerjinny@hanmail.net>
2024-08-05 16:34:38 +0900

    ãstudent 구현

---
+++ src/camp/model/Student.java
@@ -0,0 +1,53 @@
+package camp.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Student implements IStudent{
+    private String studentID; // 변경된 필드명
+    private String name;
+    private String status;
+    private Map<String, ISubject> subjects;
+
+    public Student(String sequence, String name, List<String> subjects) {
+            this.studentID = sequence;
+            this.name = name;
+            this.status = String.valueOf(status);
+            this.subjects = new HashMap<String, ISubject>();
+        }
+
+    public Student(String sequence, String studentName) {
+        this.studentID = sequence;
+    }
+
+    @Override
+        public String getStudentID () {
+            return studentID;
+        }
+
+        @Override
+        public String getName () {
+            return name;
+        }
+
+        @Override
+        public void setName (String name){
+            this.name = name;
+        }
+
+        @Override
+        public String getStatus () {
+            return status;
+        }
+
+        @Override
+        public void setStatus (String status){
+            this.status = status;
+        }
+
+        @Override
+        public Map<String, ISubject> getSubjects () {
+            return subjects;
+        }
+    }
